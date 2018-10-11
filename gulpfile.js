b1a6ad152e9a05b1725666e4ae068d3660f5505f
vini23hot <vini23hot@gmail.com>
2018-10-11 12:18:40 -0300

    Added minification for CSS and HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const env = require('gulp-environment')
 const webserver = require('gulp-webserver')
 const watch = require('gulp-watch')
 
+const cleancss = require('gulp-clean-css')
+const htmlmin = require('gulp-htmlmin')
+
 gulp.task('default', ['html', 'scss', 'js', 'img'], () => {
   if (env.is.development()) {
     gulp.start('server')
@@ -13,51 +16,58 @@ gulp.task('default', ['html', 'scss', 'js', 'img'], () => {
 
 gulp.task('html', () => {
   return gulp.src('src/**/*.html')
-    .pipe(gulp.dest('build'))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('scss', ['scss-vendor'], () => {
   return gulp.src('src/assets/scss/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('style.css'))
-    .pipe(gulp.dest('build/assets/css'))
+    .pipe(cleancss())
+    .pipe(gulp.dest('dist/assets/css'))
 })
 
 gulp.task('scss-vendor', () => {
   return gulp.src('src/assets/scss/vendor/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('build/assets/css'))
+    .pipe(cleancss())
+    .pipe(gulp.dest('dist/assets/css'))
 })
 
 gulp.task('js', ['js-vendor'], () => {
   return gulp.src('src/assets/js/*.js')
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('build/assets/js'))
+    .pipe(gulp.dest('dist/assets/js'))
 })
 
 gulp.task('js-vendor', () => {
   return gulp.src('src/assets/js/vendor/**/*.js')
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('build/assets/js'))
+    .pipe(gulp.dest('dist/assets/js'))
 })
 
 gulp.task('img', () => {
   return gulp.src('src/assets/img/**/*.*')
-  .pipe(gulp.dest('build/assets/img'))
+  .pipe(gulp.dest('dist/assets/img'))
 })
 
 gulp.task('server', ['watch'], () => {
-  return gulp.src('build')
+  return gulp.src('dist')
     .pipe(webserver({
-      livereload: true,
+      livereload: {
+        enable: true
+      },
       port: 8080,
       open: true
     }))
 })
 
 gulp.task('watch', () => {
-  watch(['src/**/*.html'], () => gulp.start('html'))
+  watch('src/**/*.html', () => gulp.start('html'))
   watch('src/assets/scss/**/*.scss', () => gulp.start('scss'))
   watch('src/assets/js/**/*.js', () => gulp.start('js'))
   watch('src/assets/imgs/**/*.*', () => gulp.start('img'))
