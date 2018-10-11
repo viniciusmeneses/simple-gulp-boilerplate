d2bf0324df2e7fb032bb6715d90f30241e85c164
vini23hot <vini23hot@gmail.com>
2018-10-11 11:16:33 -0300

    Implemented SASS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')
+const concat = require('gulp-concat')
 
-gulp.task('default', ['html', 'css', 'js', 'img'])
+gulp.task('default', ['html', 'scss'])
 
 gulp.task('html', () => {
   return gulp.src('src/**/*.html')
     .pipe(gulp.dest('build'))
+})
+
+gulp.task('scss', ['scss-vendor'], () => {
+  return gulp.src('src/assets/scss/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('build/assets/css'))
+})
+
+gulp.task('scss-vendor', () => {
+  return gulp.src('src/assets/scss/vendor/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('build/assets/css'))
 })
